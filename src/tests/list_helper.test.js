8c56b212179a6de9acd67efb727f3263a7bb4993
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-12-31 18:33:53 +0000

    Write tests for total likes and favourite blog

--- src/tests/list_helper.test.js
+++ src/tests/list_helper.test.js
@@ -5,3 +5,131 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+  test("list of blogs equals 15 likes", () => {
+    const listOfBlogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 7,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 3,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.totalLikes(listOfBlogs);
+    expect(result).toBe(15);
+  });
+
+  test("empty array returns zero", () => {
+    const emptyArray = [];
+    const result = listHelper.totalLikes(emptyArray);
+
+    expect(result).toBe(0);
+  });
+
+  test("single blog array returns likes of that blog", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 3,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(3);
+  });
+});
+
+describe("Favourite blog", () => {
+  test("find most likes in list of blogs", () => {
+    const listOfBlogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 7,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 3,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.mostLikes(listOfBlogs);
+    const expected = {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 7,
+    };
+
+    expect(result).toEqual(expected);
+  });
+
+  test("array withsingle blog returns the blog", () => {
+    const blogs = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 3,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.mostLikes(blogs);
+    const expected = {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 3,
+    };
+
+    expect(result).toEqual(expected);
+  });
+
+  test("empty array returns empty object", () => {
+    const blogs = [];
+
+    const result = listHelper.mostLikes(blogs);
+    const expected = {};
+
+    expect(result).toEqual(expected);
+  });
+});
--- src/utils/list_helper.js
+++ src/utils/list_helper.js
@@ -2,4 +2,30 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  let total = 0;
+
+  blogs.forEach((blog) => {
+    total = total + blog.likes;
+  });
+
+  return total;
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+
+  const favourite = blogs.reduce((previous, current) => {
+    return current.likes > previous.likes ? current : previous;
+  });
+
+  return {
+    title: favourite.title,
+    author: favourite.author,
+    likes: favourite.likes,
+  };
+};
+
+module.exports = { dummy, totalLikes, mostLikes };
