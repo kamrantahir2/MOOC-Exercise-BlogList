f0886ab6243554e9930e91b1fff5f0edf3127c85
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-01-04 21:08:43 +0000

    Install nross-env, specify NODE_ENV for scripts

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const password = process.env.MONGODB_URI;
+// const password = process.env.MONGODB_URI;
+const password = process.env.TEST_MONGODB_URI;
 
 export default { password };
