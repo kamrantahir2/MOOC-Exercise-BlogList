8cedca09344ec3dad9dc93bfa33e2e6dae05376c
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-01-06 16:28:21 +0000

    Write beforeEach and afterAll fucntions

--- src/tests/blog_api.test.js
+++ src/tests/blog_api.test.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
 import supertest from "supertest";
 import app from "../app.js";
+import Blog from "../models/blog.js";
+import helper from "./test_helper.js";
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
 
 describe("api tests", () => {
   test("blogs are returned as json", async () => {
@@ -11,4 +20,8 @@ describe("api tests", () => {
       .expect(200)
       .expect("Content-Type", /application\/json/);
   });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
 });
