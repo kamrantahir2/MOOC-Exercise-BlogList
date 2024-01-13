094e1d5ad0bdc6457f67a86a498cf6654d93038b
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-01-13 15:33:37 +0000

    Add username and password input fields and a submit button

--- blogListFrontend/src/App.jsx
+++ blogListFrontend/src/App.jsx
@@ -2,9 +2,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
   return (
     <>
-      <h1>TEST</h1>
+      <h2>Log in to application</h2>
+      <div>
+        <div>
+          Username:{" "}
+          <input
+            type="text"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          Password:
+          <input
+            type="password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </div>
     </>
   );
 }
