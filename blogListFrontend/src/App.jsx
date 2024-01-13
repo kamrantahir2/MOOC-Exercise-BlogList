b7102e4ba2623314cf5f8c0b75c2b4376236b37b
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-01-13 15:48:16 +0000

    Create login feature

--- blogListFrontend/src/App.jsx
+++ blogListFrontend/src/App.jsx
@@ -1,34 +1,59 @@
 import { useState } from "react";
 import "./App.css";
+import login from "./services/login.js";
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+  if (!user) {
+    return (
+      <>
+        <h3>{errorMessage}</h3>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            Username:{" "}
+            <input
+              type="text"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            Password:
+            <input
+              type="password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </>
+    );
+  }
+
   return (
-    <>
-      <h2>Log in to application</h2>
-      <div>
-        <div>
-          Username:{" "}
-          <input
-            type="text"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          Password:
-          <input
-            type="password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <div>
-          <button>Submit</button>
-        </div>
-      </div>
-    </>
+    <div>
+      <h1>You have logged in</h1>
+    </div>
   );
 }
 
--- blogListFrontend/src/services/blogs.js
+++ blogListFrontend/src/services/blogs.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3001/api/blogs";
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
 const getAll = async () => {
   const request = await axios.get(baseUrl);
   return request.data;
